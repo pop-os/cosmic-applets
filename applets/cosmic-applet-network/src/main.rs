ba677af0f486f5ea34a630117401a573f33877db
Lucy <lucy@absolucy.moe>
2022-02-01 14:10:15 -0500

    Basic app layout

--- applets/cosmic-applet-network/src/main.rs
+++ applets/cosmic-applet-network/src/main.rs
@@ -1,3 +1,22 @@
+use gtk4::{
+    gdk::Display, gio::ApplicationFlags, prelude::*, CssProvider, StyleContext,
+    STYLE_PROVIDER_PRIORITY_APPLICATION,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let application = gtk4::Application::new(
+        Some("com.system76.cosmic.applets.network"),
+        ApplicationFlags::default(),
+    );
+    application.connect_activate(build_ui);
+    application.run();
+}
+
+fn build_ui(application: &gtk4::Application) {
+    let window = gtk4::ApplicationWindow::builder()
+        .application(application)
+        .title("COSMIC Network Applet")
+        .default_width(400)
+        .default_height(600)
+        .build();
 }
