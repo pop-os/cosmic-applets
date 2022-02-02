7941c2f6a34479ed4840e78cd5998521f85289e6
Lucy <lucy@absolucy.moe>
2022-02-02 16:28:08 -0500

    Add `task:wait_for_local`

--- applets/cosmic-applet-network/src/task.rs
+++ applets/cosmic-applet-network/src/task.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 use std::future::Future;
+use tokio::sync::oneshot;
 
 pub fn spawn<O, F>(future: F) -> tokio::task::JoinHandle<O>
 where
@@ -11,4 +12,13 @@ where
 
 pub fn spawn_local<F: Future<Output = ()> + 'static>(future: F) {
     gtk4::glib::MainContext::default().spawn_local(future);
+}
+
+pub async fn wait_for_local<F: Future<Output = ()> + 'static>(future: F) {
+    let (tx, rx) = oneshot::channel::<()>();
+    gtk4::glib::MainContext::default().spawn_local(async move {
+        future.await;
+        let _ = tx.send(());
+    });
+    std::mem::drop(rx.await);
 }
