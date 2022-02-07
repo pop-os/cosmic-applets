8c376e1df2b16ddc221afc3ddeb19c57b1adb687
Lucy <lucy@absolucy.moe>
2022-02-07 11:13:13 -0500

    `task::wait_for_local` can now return a value

--- applets/cosmic-applet-network/src/task.rs
+++ applets/cosmic-applet-network/src/task.rs
@@ -14,11 +14,14 @@ pub fn spawn_local<F: Future<Output = ()> + 'static>(future: F) {
     gtk4::glib::MainContext::default().spawn_local(future);
 }
 
-pub async fn wait_for_local<F: Future<Output = ()> + 'static>(future: F) {
-    let (tx, rx) = oneshot::channel::<()>();
+pub async fn wait_for_local<O, F>(future: F) -> Option<O>
+where
+    O: Send + 'static,
+    F: Future<Output = O> + Send + 'static,
+{
+    let (tx, rx) = oneshot::channel::<O>();
     gtk4::glib::MainContext::default().spawn_local(async move {
-        future.await;
-        let _ = tx.send(());
+        std::mem::drop(tx.send(future.await));
     });
-    std::mem::drop(rx.await);
+    rx.await.ok()
 }
