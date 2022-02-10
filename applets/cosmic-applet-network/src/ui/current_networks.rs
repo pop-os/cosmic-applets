9b60cc758b20c6b79b9de05faba451c989efd9de
Lucy <lucy@absolucy.moe>
2022-02-10 14:17:20 -0500

    clean up unused imports

--- applets/cosmic-applet-network/src/ui/current_networks.rs
+++ applets/cosmic-applet-network/src/ui/current_networks.rs
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
 use cosmic_dbus_networkmanager::{
-    access_point::AccessPoint,
-    device::{wired::WiredDevice, SpecificDevice},
+    device::SpecificDevice,
     interface::enums::{ApFlags, ApSecurityFlags},
     nm::NetworkManager,
 };
