fa03b0553bfd3b9b6756aa7a28a50e97ab031715
Lucy <lucy@absolucy.moe>
2022-02-18 10:31:12 -0500

    Start on power applet

---
+++ applets/cosmic-applet-power/src/main.rs
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#[macro_use]
+extern crate relm4_macros;
+
+use gtk4::{gio::ApplicationFlags, prelude::*, Orientation, Separator};
+use once_cell::sync::Lazy;
+use tokio::runtime::Runtime;
+
+static RT: Lazy<Runtime> = Lazy::new(|| Runtime::new().expect("failed to build tokio runtime"));
+
+fn main() {
+    let application = gtk4::Application::new(
+        Some("com.system76.cosmic.applets.power"),
+        ApplicationFlags::default(),
+    );
+    application.connect_activate(build_ui);
+    application.run();
+}
+
+fn build_ui(application: &gtk4::Application) {
+    let window = gtk4::ApplicationWindow::builder()
+        .application(application)
+        .title("COSMIC Power Applet")
+        .default_width(400)
+        .default_height(300)
+        .build();
+
+    view! {
+        main_box = gtk4::Box {
+            set_orientation: Orientation::Vertical,
+            set_spacing: 10,
+            set_margin_top: 20,
+            set_margin_bottom: 20,
+            set_margin_start: 24,
+            set_margin_end: 24
+        }
+    }
+    window.set_child(Some(&main_box));
+
+    window.show();
+}
