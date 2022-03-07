4ad01331e0fab3679a4b7f2571e2c0e4d350f1b6
Lucy <lucy@absolucy.moe>
2022-03-07 15:04:14 -0500

    Perhaps we'll use relm4 for audio?

--- applets/cosmic-applet-audio/src/main.rs
+++ applets/cosmic-applet-audio/src/main.rs
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#[macro_use]
+extern crate relm4;
+
+use once_cell::sync::Lazy;
+use tokio::runtime::Runtime;
+
+static RT: Lazy<Runtime> = Lazy::new(|| Runtime::new().expect("failed to build tokio runtime"));
+
 fn main() {
     println!("Hello, world!");
 }
