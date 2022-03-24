af3470df1197d1addb9df13dbe7329592828c654
Lucy <lucy@absolucy.moe>
2022-03-24 13:35:33 -0400

    Base code for figuring out how apps work

--- applets/cosmic-applet-audio/src/main.rs
+++ applets/cosmic-applet-audio/src/main.rs
@@ -8,5 +8,15 @@ mod app;
 use relm4::RelmApp;
 
 fn main() {
+    figure_out_apps();
     RelmApp::<app::App>::new("com.system76.cosmic.applets.audio").run(());
+}
+
+fn figure_out_apps() {
+    use pulsectl::controllers::{AppControl, SinkController};
+
+    let mut sink = SinkController::create().unwrap();
+    for app in sink.list_applications().unwrap() {
+        println!("{:#?}", app);
+    }
 }
