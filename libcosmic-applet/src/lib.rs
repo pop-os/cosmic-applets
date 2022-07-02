f56b16063505a0de4e840d0488479110c43d383a
Ian Douglas Scott <idscott@system76.com>
2022-07-01 21:33:58 -0700

    Library for shared applet code; use in battery applet
    Creates window, button, and popover with correct properties and CSS
    style.

---
+++ libcosmic-applet/src/lib.rs
@@ -0,0 +1,111 @@
+use gtk4::{glib, prelude::*, subclass::prelude::*};
+use relm4_macros::view;
+
+mod deref_cell;
+use deref_cell::DerefCell;
+
+// TODO make sure style fits different panel colors?
+// TODO abstraction to start main loop? Work with relm4.
+// TODO gir bindings
+// TODO orientation, etc.
+// TODO make image size dependent on CosmicPanelConfig?
+
+static STYLE: &str = "
+window.cosmic_applet_window {
+        background: transparent;
+}
+
+button.cosmic_applet_button {
+    border-radius: 12px;
+    transition: 100ms;
+    padding: 4px;
+    border-color: transparent;
+    background: transparent;
+    outline-color: transparent;
+}
+";
+
+#[derive(Default)]
+pub struct AppletInner {
+    menu_button: DerefCell<gtk4::MenuButton>,
+    popover: DerefCell<gtk4::Popover>,
+}
+
+#[glib::object_subclass]
+impl ObjectSubclass for AppletInner {
+    const NAME: &'static str = "CosmicApplet";
+    type Type = Applet;
+    type ParentType = gtk4::Window;
+}
+
+impl ObjectImpl for AppletInner {
+    fn constructed(&self, obj: &Applet) {
+        let window = || obj;
+        view! {
+            window() {
+                add_css_class: "cosmic_applet_window",
+                set_decorated: false,
+                set_resizable: false,
+                set_width_request: 1,
+                set_height_request: 1,
+                #[wrap(Some)]
+                set_child: menu_button = &gtk4::MenuButton {
+                    add_css_class: "cosmic_applet_button",
+                    set_has_frame: false,
+                    #[wrap(Some)]
+                    set_popover: popover = &gtk4::Popover {
+                        // TODO: change if it can be positioned correctly?
+                        set_has_arrow: false,
+                    }
+                }
+            }
+        }
+
+        let provider = gtk4::CssProvider::new();
+        provider.load_from_data(STYLE.as_bytes());
+        obj.style_context().add_provider(&provider, gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+        self.menu_button.set(menu_button);
+        self.popover.set(popover);
+    }
+}
+
+impl WidgetImpl for AppletInner {}
+impl WindowImpl for AppletInner {}
+
+glib::wrapper! {
+    pub struct Applet(ObjectSubclass<AppletInner>)
+        @extends gtk4::Widget, gtk4::Window;
+}
+
+impl Default for Applet {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Applet {
+    pub fn new() -> Self {
+        glib::Object::new(&[]).unwrap()
+    }
+
+    fn inner(&self) -> &AppletInner {
+        AppletInner::from_instance(self)
+    }
+
+    pub fn set_button_child(&self, child: Option<&impl IsA<gtk4::Widget>>) {
+        self.inner().menu_button.set_child(child);
+    }
+
+    pub fn set_button_icon_name(&self, name: &str) {
+        self.inner().menu_button.set_icon_name(name);
+    }
+
+    pub fn set_button_label(&self, label: &str) {
+        self.inner().menu_button.set_label(label);
+    }
+
+    pub fn set_popover_child(&self, child: Option<&impl IsA<gtk4::Widget>>) {
+        self.inner().popover.set_child(child);
+    }
+}
