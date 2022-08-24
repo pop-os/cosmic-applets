2485511a4922e6fdb7baa90e2786547391be8e2f
13r0ck <bnr@tuta.io>
2022-08-23 18:18:24 -0600

    Reset cosmic-applet-audio

--- applets/cosmic-applet-audio/src/volume.rs
+++
@@ -1,9 +0,0 @@
-use gtk4::prelude::*;
-use libpulse_binding::volume::Volume;
-
-use crate::{pa::DeviceInfo, volume_scale::VolumeScale};
-
-pub fn update_volume(device: &DeviceInfo, scale: &VolumeScale) {
-    scale.set_name(device.name.clone());
-    scale.set_volume(&device.volume);
-}
