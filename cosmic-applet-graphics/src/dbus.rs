28446a33ab73a20eee5af2c1202df53a162edb03
Ashley Wulber <ashley.wulber@pm.me>
2023-08-30 19:25:44 -0400

    fix(graphics): exit if connection to com.system76.PowerDaemon fails

--- cosmic-applet-graphics/src/dbus.rs
+++ cosmic-applet-graphics/src/dbus.rs
@@ -89,5 +89,9 @@ pub async fn init() -> Option<(Connection, PowerDaemonProxy<'static>)> {
         _ => return None,
     };
 
+    if matches!(proxy.introspect().await, Err(zbus::fdo::Error::ServiceUnknown(_))) {
+        return None;
+    }
+
     Some((conn, proxy))
 }
