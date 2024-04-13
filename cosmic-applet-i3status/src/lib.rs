c7c0c70a97ebb44f8f5b693dde6509679762593f
Ron Waldon-Howe <jokeyrhyme@jokeyrhy.me>
2024-04-13 14:31:25 +1000

    feat(i3status): new applet to embed `i3status` in panel/dock (blocks)

--- cosmic-applet-i3status/src/lib.rs
+++ cosmic-applet-i3status/src/lib.rs
@@ -5,13 +5,13 @@ use crate::localize::localize;
 use cosmic::app::Command;
 use cosmic::applet::cosmic_panel_config::PanelAnchor;
 use cosmic::iced::Length;
-
 use cosmic::iced_futures::Subscription;
 use cosmic::iced_style::application;
 use cosmic::iced_widget::{Column, Row};
-
 use cosmic::{Element, Theme};
-use subprocess::Message;
+use subprocess::Output;
+use swaybar_types::Block;
+use tracing::{span, Level};
 
 pub fn run() -> cosmic::iced::Result {
     localize();
@@ -20,21 +20,23 @@ pub fn run() -> cosmic::iced::Result {
 
 #[derive(Default)]
 struct I3status {
+    blocks: Vec<Block>,
     core: cosmic::app::Core,
-    msg: String,
+    text: String,
 }
 
 impl cosmic::Application for I3status {
-    type Message = Message;
+    type Message = Output;
     type Executor = cosmic::SingleThreadExecutor;
     type Flags = ();
     const APP_ID: &'static str = "com.system76.CosmicAppletI3status";
 
-    fn init(core: cosmic::app::Core, _flags: ()) -> (Self, Command<Message>) {
+    fn init(core: cosmic::app::Core, _flags: ()) -> (Self, Command<Output>) {
         (
             Self {
+                blocks: vec![],
                 core,
-                msg: String::from("i3status here"),
+                text: String::new(),
             },
             Command::none(),
         )
@@ -52,24 +54,43 @@ impl cosmic::Application for I3status {
         Some(cosmic::applet::style())
     }
 
-    fn update(&mut self, message: Message) -> Command<Message> {
+    fn update(&mut self, message: Output) -> Command<Output> {
+        let span = span!(Level::TRACE, "I3status::update()");
+        let _ = span.enter();
         match message {
-            Message::Output(msg) => self.msg = msg,
-            Message::Error(msg) => self.msg = msg,
+            Output::Blocks(blocks) => {
+                self.blocks = blocks;
+                self.text = String::new();
+            }
+            Output::Raw(output) => {
+                self.blocks = vec![];
+                self.text = output;
+            }
+            Output::None => {}
         }
         Command::none()
     }
 
-    fn subscription(&self) -> Subscription<Message> {
-        subprocess::start()
+    fn subscription(&self) -> Subscription<Output> {
+        let span = span!(Level::TRACE, "I3status::subscription()");
+        let _ = span.enter();
+        subprocess::child_process()
     }
 
-    fn view(&self) -> Element<Message> {
+    fn view(&self) -> Element<Output> {
         let theme = self.core.system_theme().cosmic();
         let space_xxs = theme.space_xxs();
 
-        let label = cosmic::iced_widget::text(&self.msg);
-        let children = vec![label.into()];
+        let children = if !self.blocks.is_empty() {
+            self.blocks
+                .iter()
+                .map(|block| cosmic::iced_widget::text(&block.full_text).into())
+                .collect::<Vec<Element<Output>>>()
+        } else if !self.text.is_empty() {
+            vec![cosmic::iced_widget::text(&self.text).into()]
+        } else {
+            vec![cosmic::iced_widget::text("no output").into()]
+        };
 
         if matches!(
             self.core.applet.anchor,
--- cosmic-applet-i3status/src/subprocess.rs
+++ cosmic-applet-i3status/src/subprocess.rs
@@ -1,61 +1,126 @@
-use std::{any::TypeId, process::Stdio};
+use std::{any::TypeId, io, process::Stdio, time::Duration};
 
-use cosmic::{iced::subscription, iced_futures::futures::SinkExt};
+use cosmic::iced_futures::futures::future;
+use swaybar_types::Block;
 use tokio::{
-    io::{AsyncBufReadExt, BufReader},
-    process::Command,
+    io::{AsyncBufReadExt, BufReader, Lines},
+    process::{Child, ChildStdout, Command},
+    time::sleep,
 };
-
-const CHANNEL_SIZE: usize = 1;
 
 // TODO: support auto-detection of `i3status` and `i3status-rs` executables
 // TODO: support basename-only commands by searching PATH (using "which" crate?)
 // TODO: provide GUI for user to add their own preferred command
 const COMMAND: &str = "/usr/bin/i3status-rs";
 
-pub fn start() -> cosmic::iced::Subscription<Message> {
-    struct Worker;
-    subscription::channel(
-        TypeId::of::<Worker>(),
-        CHANNEL_SIZE,
-        |mut output| async move {
-            loop {
-                let mut cmd = Command::new(COMMAND)
-                    .args(["--version"])
-                    .stdout(Stdio::piped())
-                    .spawn()
-                    .expect("{command} should start");
-                let stdout = cmd
-                    .stdout
-                    .as_mut()
-                    .expect("should capture stdout from {command}");
-                let reader = BufReader::new(stdout);
-                let mut lines = reader.lines();
-                while let Some(line) = lines
-                    .next_line()
-                    .await
-                    .expect("should read from buffered stdout")
-                {
-                    output
-                        .send(Message::Output(line))
-                        .await
-                        .expect("should send stdout line");
-                }
+fn spawn() -> io::Result<Child> {
+    Command::new(COMMAND)
+        // `.stdin(Stdio::null())` is faster, but `i3status-rs` requires a working stdio
+        // (and the i3bar protocol doesn't specify either way)
+        // TODO: support click events on blocks
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+}
 
-                // effectively end the subscription by never sending more values
-                cosmic::iced::futures::future::pending().await
+async fn read_blocks(state: State) -> (Output, State) {
+    match state {
+        State::Ready => match spawn() {
+            Ok(mut child) => {
+                let stdout = child
+                    .stdout
+                    .take()
+                    .expect("should capture stdout from i3status");
+                let reader = BufReader::new(stdout);
+                let stdout_lines = reader.lines();
+                (
+                    Output::Raw(String::from("i3status started")),
+                    State::Running {
+                        child,
+                        stdout_lines,
+                    },
+                )
             }
+            Err(_) => (
+                Output::Raw(String::from("cannot spawn i3status")),
+                State::Finished,
+            ),
         },
-    )
+        State::Running {
+            child,
+            mut stdout_lines,
+        } => {
+            match stdout_lines.next_line().await {
+                Ok(Some(line)) => {
+                    // for more information about the protocol,
+                    // see: https://i3wm.org/docs/i3bar-protocol.html
+
+                    // the "endless array" output means we have a dangling comma to remove
+                    let line = line.trim_end_matches(',');
+
+                    if let Ok(blocks) = serde_json::from_str::<Vec<Block>>(line) {
+                        (
+                            Output::Blocks(blocks),
+                            State::Running {
+                                child,
+                                stdout_lines,
+                            },
+                        )
+                    } else {
+                        (
+                            Output::Raw(String::from(line)),
+                            State::Running {
+                                child,
+                                stdout_lines,
+                            },
+                        )
+                    }
+                }
+                Ok(None) => {
+                    sleep(Duration::from_secs(3)).await;
+                    (
+                        Output::None,
+                        State::Running {
+                            child,
+                            stdout_lines,
+                        },
+                    )
+                }
+                Err(_) => (
+                    Output::Raw(String::from("cannot read i3status stdout")),
+                    State::Finished,
+                ),
+            }
+        }
+        State::Finished => {
+            // We do not let the stream die, as it would start a
+            // new download repeatedly if the user is not careful
+            // in case of errors.
+            future::pending().await
+        }
+    }
+}
+
+pub fn child_process() -> cosmic::iced::Subscription<Output> {
+    struct SomeWorker;
+    cosmic::iced::subscription::unfold(TypeId::of::<SomeWorker>(), State::Ready, |state| {
+        read_blocks(state)
+    })
 }
 
 #[derive(Clone, Debug)]
-pub enum Message {
-    Output(String),
-    Error(String),
+pub enum Output {
+    Blocks(Vec<Block>),
+    Raw(String),
+    None,
 }
 
-enum State {
-    Starting,
-    Ready(),
+#[derive(Debug)]
+pub enum State {
+    Ready,
+    Running {
+        child: Child,
+        stdout_lines: Lines<BufReader<ChildStdout>>,
+    },
+    Finished,
 }
