8b1fffe1d0222fd2c280bee16119115b25480141
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-06-09 00:29:02 +0200

    remove journald, remove logs, update fde

--- cosmic-app-list/src/main.rs
+++ cosmic-app-list/src/main.rs
@@ -1,28 +1,13 @@
 // Copyright 2023 System76 <info@system76.com>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use tracing::{info, warn};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
-
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() -> cosmic::iced::Result {
-    init_logging();
+    tracing_subscriber::fmt::init();
+    let _ = tracing_log::LogTracer::init();
 
-    warn!("hello");
-    
+    tracing::info!("Starting cosmic-app-list with version {VERSION}");
+
     cosmic_app_list::run()
-}
-
-fn init_logging() {
-    if let Ok(journal_layer) = tracing_journald::layer() {
-        let filter_layer = EnvFilter::try_from_default_env()
-            .or_else(|_| EnvFilter::try_new("info"))
-            .unwrap();
-
-        tracing_subscriber::registry()
-            .with(filter_layer)
-            .with(journal_layer)
-            .init()
-    }
 }
