e4276f15d6d512ffcc8e53a6db1a88e85723a3e3
Ian Douglas Scott <idscott@system76.com>
2024-06-13 15:45:59 -0700

    battery: Use `upower_dbus` instead of including bindings here

--- cosmic-applet-battery/src/upower_kbdbacklight.rs
+++ cosmic-applet-battery/src/upower_kbdbacklight.rs
@@ -6,31 +6,8 @@
 use cosmic::iced::{self, futures::SinkExt, subscription};
 use std::{fmt::Debug, hash::Hash};
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
-use zbus::proxy;
 
-#[proxy(
-    default_service = "org.freedesktop.UPower",
-    interface = "org.freedesktop.UPower.KbdBacklight",
-    default_path = "/org/freedesktop/UPower/KbdBacklight"
-)]
-trait KbdBacklight {
-    /// GetBrightness method
-    fn get_brightness(&self) -> zbus::Result<i32>;
-
-    /// GetMaxBrightness method
-    fn get_max_brightness(&self) -> zbus::Result<i32>;
-
-    /// SetBrightness method
-    fn set_brightness(&self, value: i32) -> zbus::Result<()>;
-
-    /// BrightnessChanged signal
-    #[zbus(signal)]
-    fn brightness_changed(&self, value: i32) -> zbus::Result<()>;
-
-    /// BrightnessChangedWithSource signal
-    #[zbus(signal)]
-    fn brightness_changed_with_source(&self, value: i32, source: &str) -> zbus::Result<()>;
-}
+use upower_dbus::KbdBacklightProxy;
 
 pub fn kbd_backlight_subscription<I: 'static + Hash + Copy + Send + Sync + Debug>(
     id: I,
