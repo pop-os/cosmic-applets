e51ca81f7dc3e3e40674cde85de74081035dd728
lazytanuki <43273245+lazytanuki@users.noreply.github.com>
2024-08-13 19:03:34 +0200

    feat: toggle popups on press down

--- cosmic-applet-network/src/app.rs
+++ cosmic-applet-network/src/app.rs
@@ -543,7 +543,7 @@ impl cosmic::Application for CosmicNetworkApplet {
         self.core
             .applet
             .icon_button(&self.icon_name)
-            .on_press(Message::TogglePopup)
+            .on_press_down(Message::TogglePopup)
             .into()
     }
 
