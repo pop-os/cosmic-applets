f486b3b1ad450fbb20a94f2edf3314b41334f33e
Ashley Wulber <ashley@system76.com>
2024-12-02 09:58:14 -0500

    update libcosmic

--- cosmic-applet-audio/src/lib.rs
+++ cosmic-applet-audio/src/lib.rs
@@ -376,7 +376,7 @@ impl cosmic::Application for Audio {
 
                 self.output_volume_debounce = true;
 
-                return cosmic::command::future(async move {
+                return cosmic::task::future(async move {
                     tokio::time::sleep(Duration::from_millis(64)).await;
                     Message::ApplyOutputVolume
                 });
@@ -395,7 +395,7 @@ impl cosmic::Application for Audio {
 
                 self.input_volume_debounce = true;
 
-                return cosmic::command::future(async move {
+                return cosmic::task::future(async move {
                     tokio::time::sleep(Duration::from_millis(64)).await;
                     Message::ApplyInputVolume
                 });
