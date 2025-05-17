212493433e84525d7d1c32d2d47c82bc8795aa8d
sandptel <sandeeppatel626735@gmail.com>
2025-05-17 18:40:20 +0530

    feat: updated flake.nix and fixed error due to crane api change

--- flake.nix
+++ flake.nix
@@ -19,7 +19,8 @@
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        craneLib = crane.lib.${system}.overrideToolchain fenix.packages.${system}.stable.toolchain;
+
+	craneLib = (crane.mkLib pkgs).overrideToolchain fenix.packages.${system}.stable.toolchain;
 
         pkgDef = {
           pname = "cosmic-applets";
